c62b342e326d51d99be6d32bc0fedf9a51763186
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2024-11-18 10:36:31 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import Index from "./pages/Index";
+import logo from './assets/logo-1.png'
+import Login from "./components/Login";
 
 function App() {
   return (
-   <Routes>
-    <Route path="/" element={<Index />} />
-    <Route path="/login" element={<Index />} />
-   </Routes>
+    <div className="flex w-full h-screen">
+    <div className="w-full flex items-center justify-center lg:w-2/3">
+      <Login />
+    </div>
+    <div className="hidden shadow-sm relative lg:flex h-full w-1/3 items-center justify-center bg-white">
+      {/* <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full" /> */}
+      {/* <div className="w-full h-1/2 bg-white/10 backdrop-blur-lg" /> */}
+      {/* <div className="w-full h-1/2 bg-white/10 backdrop-blur-lg"/> */}
+      <img src={logo} alt="Logo" />
+    </div>
+  </div>
   );
 }
 
