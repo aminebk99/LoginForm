302f4e81d6a118dd3d752ff6ab62c16131dad899
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2024-11-18 10:37:26 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,6 @@ function App() {
       <Login />
     </div>
     <div className="hidden shadow-sm relative lg:flex h-full w-1/3 items-center justify-center bg-white">
-      {/* <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full" /> */}
-      {/* <div className="w-full h-1/2 bg-white/10 backdrop-blur-lg" /> */}
-      {/* <div className="w-full h-1/2 bg-white/10 backdrop-blur-lg"/> */}
       <img src={logo} alt="Logo" />
     </div>
   </div>
