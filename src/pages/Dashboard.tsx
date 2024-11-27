c22fc699e53854b97641f7151faad3bfa0c921a6
Mohamed Amine Bazaou <129026688+aminebk99@users.noreply.github.com>
2024-11-27 17:01:38 +0100

    remove comment

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,39 +1,37 @@
 import { useEffect, useState } from 'react';
-import axios from 'axios'; // Import axios
+import axios from 'axios'; 
 
-// Define an interface for the 'data' structure
 interface UserData {
   username: string;
-  // Add other properties if needed, like:
   email: string;
-  // age: number;
 }
 
 function Dashboard() {
-  // Define the type of 'data' state as UserData | null (since the data could be initially empty)
   const [data, setData] = useState<UserData | null>(null); 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const res = await axios.get("http://localhost:8082/users/data", {
-            withCredentials: true, // Set this to true if you need to include credentials
+            withCredentials: true,
         });
-        setData(res.data); // Assuming the response contains data in the 'UserData' format
+        setData(res.data); 
       } catch (error) {
-        console.error("Error fetching data:", error); // Error handling
+        console.error("Error fetching data:", error); 
       }
     };
 
-    fetchData(); // Calling fetchData inside useEffect
-  }, []); // Empty dependency array means this effect runs once when the component mounts
+    fetchData(); 
+  }, []); 
 
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      {/* Render the username if 'data' is not null */}
+        <div className=' flex flex-col'>
       <h2 className='text-3xl'>{data ? `Welcome ${data.username}` : 'Loading...'}</h2>
-      <br />
       <span>{data?.email}</span>
+
+        </div>
+      
     </div>
   );
 }
